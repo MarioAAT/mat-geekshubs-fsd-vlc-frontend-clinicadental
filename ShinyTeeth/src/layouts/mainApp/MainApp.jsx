cae12e7363e173b4bd243ec2f5f367c022b8e46e
Mario Aguilar <marioaguilar740@gmail.com>
2023-03-13 22:50:06 +0100

    routing of the login page

--- ShinyTeeth/src/layouts/mainApp/MainApp.jsx
+++ ShinyTeeth/src/layouts/mainApp/MainApp.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { NavBar } from '../../components/NavBar';
 import { Home } from '../home/Home';
+import { Login } from '../Login/Login';
 import { Register } from '../register/Register';
 
 export const MainApp = () => {
@@ -10,6 +11,7 @@ export const MainApp = () => {
     <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/register' element={<Register/>}/>
+        <Route path='/login' element={<Login/>}/>
     </Routes>
     </>
   )
