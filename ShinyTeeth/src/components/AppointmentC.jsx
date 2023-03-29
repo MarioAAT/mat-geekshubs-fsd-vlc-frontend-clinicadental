c24a1c908dfb20223ad98ba74bc8d1f8d02bc217
Mario Aguilar <marioaguilar740@gmail.com>
2023-03-29 18:31:05 +0200

    we managed to obtain all the appointments successfully and we apply a ternary in the appoinmentC component

--- ShinyTeeth/src/components/AppointmentC.jsx
+++ ShinyTeeth/src/components/AppointmentC.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 import Card from 'react-bootstrap/Card';
+import { useSelector } from "react-redux";
+import { userData } from "../layouts/userSlice";
 
 
 //   export const CardAppointment = ({appo}) => {
 function CardAppointment ({appo})  {
+    const credentialsRdx = useSelector(userData)
 
     return (
+        <>
         <Card style={{ width: '18rem' }}>
         <Card.Body>
             <Card.Title>{appo.date}</Card.Title>
                 <ul>
+                {credentialsRdx.credentials.roleId === 3 ? (
+                    <>
+                    <li><li>Tratamiento:</li>{appo.treatment_id}</li>
+                    <li><li>Doctor:</li>{appo.professional_id}</li>
+                    <li><li>Paciente:</li>{appo.patient_id}</li>
                     <li><li>Dia:</li>{appo.appointment_on}</li>
                     <li><li>Hora:</li>{appo.start_at}</li>
-                    {/* <li><li>Tratamiento:</li>{appo.treatment_id}</li>
-                    <li><li>Doctor:</li>{appo.professional_id}</li> */}
+                    </>
+                    ): credentialsRdx.credentials.roleId === 4? (
+                    <>
+                    <li><li>Tratamiento:</li>{appo.treatment_id}</li>
+                    <li><li>Doctor:</li>{appo.professional_id}</li>
+                    <li><li>Paciente:</li>{appo.patient_id}</li>
+                    <li><li>Dia:</li>{appo.appointment_on}</li>
+                    <li><li>Hora:</li>{appo.start_at}</li>
+                    </>
+                    ) : (
+                    <>
+                    <li><li>Dia:</li>{appo.appointment_on}</li>
+                    <li><li>Hora:</li>{appo.start_at}</li>
+                    </>
+                    )}
                 </ul>
         </Card.Body>
         </Card>
+        </>
     );
 }
     
--- ShinyTeeth/src/layouts/appointment/AllAppointment.jsx
+++ ShinyTeeth/src/layouts/appointment/AllAppointment.jsx
@@ -1,13 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { addChoosen } from '../detailSlice';
 import { userData } from '../userSlice';
+import { Col, Container, Row } from "react-bootstrap";
+import CardAppointment from '../../components/AppointmentC';
 import { getAllAppointments } from '../../services/apiCalls';
 
 
 export const AllAppointment = () => {
+
+    const [allAppointments, setAllAppointments] = useState ([]);
+    const ReduxCredentials = useSelector(userData)
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(allAppointments.length === 0) {
+            getAllAppointments(ReduxCredentials?.credentials?.token)
+            .then((result) => {
+                setAllAppointments(result.data.appointments);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        }
+    }, [allAppointments]);
+
+
   return (
-    <div>AllAppointment</div>
+    <>
+        <Container>
+        { allAppointments.length > 0 ? 
+            (<Row>
+                {allAppointments.map( (citas) => {
+                    return (
+                        <Col key={citas.id}>
+                            <CardAppointment appo={citas}/>
+                        </Col>
+                    );
+                })}
+            </Row>)
+            : 
+            (<div>......</div>)
+        }        
+        </Container>
+    </>
   )
 }
